264f828567338cd4a093551f57d445e10756d3ed
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-18 23:23:58 +0900

    specify port by env

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
 import express from "express";
+import { boardRouter } from "./infrastructure/router";
+import { BoardController } from "./presentation/BoardController";
 
 const app = express();
 
+app.use(boardRouter);
+
 app.get("/", (_request, response) => {
-  return response.json({ message: "Hello DDD" });
+  return response.json({
+    message: "Hello World",
+    port: process.env.PORT,
+  });
 });
 
-app.listen(3000, () => {
-  console.log("listen on 300 PORT");
+const controller = new BoardController();
+
+app.get("/boards/:id", (_request, response) => {
+  const result = controller.getBoard();
+  return response.json({ message: "board", result });
+});
+
+app.listen(Number(process.env.PORT), () => {
+  console.log(`listen on ${process.env.PORT} PORT`);
 });
 
 export default app;
