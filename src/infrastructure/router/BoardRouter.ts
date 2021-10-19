af6775461e5c2a70a1fb3861a302932149105c4b
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-20 08:27:44 +0900

    create auth middleware base

--- src/infrastructure/router/BoardRouter.ts
+++ src/infrastructure/router/BoardRouter.ts
@@ -5,11 +5,18 @@ export const boardRouter = Router();
 
 const controller = new BoardController();
 
-boardRouter.get("/boards/:id", (request, response, next) => {
+boardRouter.use((request, response, next) => {
   (async () => {
     /**
      * TODO: CookieUserAuthenticationService.authenticateFromを呼び出す
      */
+    console.log("Time:", Date.now());
+    next();
+  })().catch(next);
+});
+
+boardRouter.get("/boards/:id", (request, response, next) => {
+  (async () => {
     const result = await controller.getBoard(Number(request.params.id));
     return response.json({ message: "board", result });
   })().catch(next);
