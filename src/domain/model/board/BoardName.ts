bf9083b0bfc213c137c0517075546a2fc9c72ab8
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-20 08:07:14 +0900

    fix BoardName VO

--- src/domain/model/board/BoardName.ts
+++ src/domain/model/board/BoardName.ts
@@ -1,11 +1,15 @@
 import { ValueObject } from "../../../ddd_common/domain/ValueObject";
 
 export class BoardName extends ValueObject<BoardName> {
+  public value: string;
+
   constructor(name: string) {
     super();
 
     if (name.length < 5 || name.length > 100) {
       throw new Error("ボード名の長さが不正です");
     }
+
+    this.value = name;
   }
 }
