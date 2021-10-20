8b84b8ea8796d0a519df6226568ae38eeb5d5904
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-10-20 23:14:46 +0900

    create addTaskToBoard method to controller

--- src/presentation/BoardController.ts
+++ src/presentation/BoardController.ts
@@ -1,16 +1,36 @@
+import { BoardRepository } from "../domain/model/board/BoardRepository";
+import { UserRepository } from "../domain/model/user/UserRepository";
+import { AddTaskToBoardCommandHandler, CreateTaskCommand } from "../usecase/command/AddTaskToBoardCommandHandler";
 import { GetBoardQueryHandler } from "../usecase/query/GetBoardQueryHandler";
 import { BoardView } from "../usecase/query/view/BoardView";
 
 export class BoardController {
   private getBoardQueryHandler: GetBoardQueryHandler;
+  private boardRepository: BoardRepository;
+  private userRepository: UserRepository;
 
-  constructor(getBoardQueryHandler: GetBoardQueryHandler) {
+  constructor(
+    getBoardQueryHandler: GetBoardQueryHandler,
+    boardRepository: BoardRepository,
+    userRepository: UserRepository,
+  ) {
     this.getBoardQueryHandler = getBoardQueryHandler;
+    this.boardRepository = boardRepository;
+    this.userRepository = userRepository;
   }
 
   async getBoard(boardId: number): Promise<BoardView> {
     const result = this.getBoardQueryHandler.handle(boardId);
 
     return result;
+  }
+
+  async addTaskToBoard(command: CreateTaskCommand): Promise<void> {
+    const usecase = new AddTaskToBoardCommandHandler(
+      this.boardRepository,
+      this.userRepository,
+    );
+    
+    await usecase.handle(command);
   }
 }
