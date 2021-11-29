eb8fa10e652fcc564f4f837ccafc4dab8fdec122
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-11-29 17:46:30 +0900

    シードデータにレーンを追加

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,70 +1,85 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 async function main() {
   const bob = await prisma.users.create({
     data: {
-      name: "bob",
-      icon: "https://example.con/bob-icon.png",
-      hashed_password: "password",
-      email: "bob@example.com"
+      name: 'bob',
+      icon: 'https://example.con/bob-icon.png',
+      hashed_password: 'password',
+      email: 'bob@example.com',
     },
-  });
+  })
   const sampleBoard = await prisma.boards.create({
     data: {
-      name: "サンプルボード"
-    }
-  });
+      name: 'サンプルボード',
+    },
+  })
   await prisma.participants.create({
     data: {
       user_id: bob.id,
       board_id: sampleBoard.id,
-    }
-  });
+    },
+  })
+  const lane = await prisma.lanes.findFirst({
+    where: {
+      board: sampleBoard,
+    },
+  })
   await prisma.tasks.create({
     data: {
-      name: "Fix CSS bug",
+      name: 'Fix CSS bug',
       deadline: new Date(),
-      content: "Please fix CSS layout bug as soon as possible",
+      content: 'Please fix CSS layout bug as soon as possible',
       point: 5,
+      lane: {
+        connect: {
+          id: lane.id,
+        },
+      },
       board: {
         connect: {
           id: sampleBoard.id,
-        }
+        },
       },
       assigned_user: {
         connect: {
           id: bob.id,
-        }
-      }
+        },
+      },
     },
-  });
+  })
   await prisma.tasks.create({
     data: {
-      name: "create new function",
+      name: 'create new function',
       deadline: new Date(),
-      content: "user need",
+      content: 'user need',
       point: 2,
+      lane: {
+        connect: {
+          id: lane.id,
+        },
+      },
       board: {
         connect: {
           id: sampleBoard.id,
-        }
+        },
       },
       assigned_user: {
         connect: {
           id: bob.id,
-        }
-      }
+        },
+      },
     },
-  });
+  })
 }
 
 main()
-  .catch(e => {
-    console.error(e);
-    process.exit(1);
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
   })
   .finally(async () => {
-    await prisma.$disconnect();
-  });
+    await prisma.$disconnect()
+  })
