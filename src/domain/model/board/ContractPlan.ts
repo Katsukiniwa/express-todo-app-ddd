92e978372b2039caab09bc1c2aa3beb9a419887e
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-04-13 11:55:46 +0900

    add contract plan enum

---
+++ src/domain/model/board/ContractPlan.ts
@@ -0,0 +1,1 @@
+export type ContractPlan = 'FREE' | 'BUSINESS' | 'ENTERPRISE'
--- src/domain/model/board/Owner.ts
+++ src/domain/model/board/Owner.ts
@@ -1,9 +1,11 @@
 import { Entity } from '../../../ddd_common/domain/Entity'
+import { ContractPlan } from './ContractPlan'
 
 export interface OwnerProps {
   id: number | null
   name: string
   icon: string
+  contractPlan: ContractPlan
 }
 
 /**
@@ -14,12 +16,14 @@ export class Owner extends Entity<Owner> {
   public readonly id: number | null
   private _name: string
   private _icon: string
+  private _contractPlan: ContractPlan
 
   constructor(props: OwnerProps) {
     super()
     this.id = props.id
     this._name = props.name
     this._icon = props.icon
+    this._contractPlan = props.contractPlan
   }
 
   get name(): string {
@@ -28,5 +32,9 @@ export class Owner extends Entity<Owner> {
 
   get icon(): string {
     return this._icon
+  }
+
+  get contractPlan(): ContractPlan {
+    return this._contractPlan
   }
 }
